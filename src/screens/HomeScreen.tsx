884fbe79c69275d7316b85bb22070986ae444c43
Adrián Coll <acoll@aiudo.es>
2022-05-06 00:16:21 +0200

    feat: first dispatch

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,33 @@
 import { RootState } from '@store'
 import { StatusBar } from 'expo-status-bar'
+import { useAppDispatch } from 'hooks/redux'
+import { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { useSelector } from 'react-redux'
-import { UserState } from 'store/features/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser, UserState } from '../store/features/user/userSlice'
 
 export default function HomeScreen() {
   const { user } = useSelector<RootState, UserState>((state) => state.user)
+  const dispatch = useDispatch()
+
+  const handlePress = () => {
+    dispatch(
+      setUser({
+        name: 'asdasd',
+        phoneNumber: 'string',
+        email: 'test@asd.asd',
+        image: 'string',
+        role: 'BUSINESS_ROLE',
+        status: true,
+        google: true,
+        uid: 'asdasd',
+      })
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{user.email}</Text>
+      <Text onPress={handlePress}>{user.email}</Text>
       <StatusBar style='auto' />
     </View>
   )
