647da9b5d8e2c105d85f53404ed678c6affb4791
Adrián Coll <acoll@aiudo.es>
2022-05-05 19:38:15 +0200

    feat: login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native'
+import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg'
+import SvgTop from './components/auth/login/SvgTop'
+
+const { width, height } = Dimensions.get('window')
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Stylish!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.containerSVG}>
+        <SvgTop />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Hola</Text>
+        <Text style={styles.subTitle}>Inicia sesión con tu cuenta</Text>
+        <TextInput placeholder='jhon@stylish.com' style={styles.textInput} />
+        <TextInput
+          placeholder='contraseña'
+          style={styles.textInput}
+          secureTextEntry={true}
+        />
+        <Text style={styles.forgotPassword}>He olvidado mi contraseña</Text>
+        <Text style={styles.forgotPassword}>¿No tienes una cuenta?</Text>
+        <StatusBar style='auto' />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    backgroundColor: '#f1f1f1',
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30
   },
-});
+  containerSVG: {
+    width: width,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  titulo: {
+    fontSize: 80,
+    color: '#34434D',
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 30,
+    width: '100%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+  },
+  forgotPassword: {
+    fontSize: 14,
+    alignSelf: 'flex-end',
+    color: 'gray',
+    marginTop: 20,
+  },
+  button: {},
+})
