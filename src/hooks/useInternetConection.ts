4c654d6b5600da2d022c726685f2962fdf56babe
adriancoll <acoll@aiudo.es>
2022-05-20 17:56:09 +0200

    use internet connection package

---
+++ src/hooks/useInternetConection.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+/**
+ * @see https://blog.logrocket.com/managing-network-connection-status-in-react-native/
+ * @returns {boolean}
+ */
+export const useInternetConnection = () => {
+  const [isOffline, setOfflineStatus] = useState(false);
+
+  useEffect(() => {
+    const removeNetInfoSubscription = NetInfo.addEventListener((state) => {
+      const offline = !(state.isConnected && state.isInternetReachable);
+      setOfflineStatus(offline);
+    });
+
+
+    return () => removeNetInfoSubscription();
+  }, []);
+
+  return isOffline
+};
