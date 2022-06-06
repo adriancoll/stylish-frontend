9c88369591ef466f75219da2c342e52165ff1ef8
adriancoll <acoll@aiudo.es>
2022-06-06 12:20:01 +0200

    feat: agenda working

--- src/components/main/AgendaScreen/AgendaComponent.tsx
+++ src/components/main/AgendaScreen/AgendaComponent.tsx
@@ -1,4 +1,5 @@
-import { useTheme } from "@react-navigation/native";
+import { useNavigation, useTheme } from "@react-navigation/native";
+import * as Animatable from 'react-native-animatable'
 import { isEmpty } from "lodash";
 import moment from "moment";
 import React, { FC, useEffect, useState } from "react";
@@ -11,6 +12,7 @@ import {
   RefreshControl,
   useColorScheme,
   ToastAndroid,
+  Dimensions,
 } from "react-native";
 import {
   Agenda,
@@ -26,6 +28,11 @@ import { getMyAppointments } from "../../../store/features/appointments/appointm
 import { AppointmentsState } from "../../../store/features/appointments/appointmentSlice";
 import theme from "../../../theme/theme";
 import { EvilIcons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { UserState } from "../../../store/features/user/userSlice";
+import AnimatedLottieView from "lottie-react-native";
+import TouchableScale from "react-native-touchable-scale";
+import { DELAY } from "../../../constants/animations";
 
 LocaleConfig.locales["es"] = {
   monthNames: [
@@ -70,11 +77,19 @@ LocaleConfig.locales["es"] = {
 };
 LocaleConfig.defaultLocale = "es";
 
+const { width, height } = Dimensions.get("window");
+
 interface Props {}
 
 const SEPARATOR = "|";
 
+type AgendaScreenProp = NativeStackNavigationProp<RootStackParamList, "Agenda">;
+
 const AgendaComponent: FC<Props> = ({}) => {
+  const today = moment().format("YYYY-MM-DD");
+
+  const navigator = useNavigation<AgendaScreenProp>();
+
   const [isLoading, setisLoading] = useState(false);
   const [parsedAppointments, setParsedAppointments] = useState<AgendaSchedule>(
     {}
@@ -86,6 +101,10 @@ const AgendaComponent: FC<Props> = ({}) => {
 
   const { appointments } = useSelector<RootState, AppointmentsState>(
     (state) => state.appointments
+  );
+
+  const { isBusiness } = useSelector<RootState, UserState>(
+    (state) => state.user
   );
 
   useEffect(() => {
@@ -106,10 +125,16 @@ const AgendaComponent: FC<Props> = ({}) => {
           data[day] = [];
         }
 
+        console.log(appointmentItem.business );
+
         data[day].push({
           name: `${moment(appointmentItem.date).format("HH:MM")} - ${
             appointmentItem.service_type.name
-          }|${appointmentItem.business.name}`,
+          }|${
+            isBusiness
+              ? appointmentItem.user.name
+              : appointmentItem.business.name
+          }|${JSON.stringify(appointmentItem.business)}`,
           height: 50,
           day,
         });
@@ -123,36 +148,53 @@ const AgendaComponent: FC<Props> = ({}) => {
     const fontSize = isFirst ? 16 : 14;
     const color = isFirst ? colors.primary : theme.colors["primary-light"];
 
-    const data = reservation.name.split(SEPARATOR);
+    const [title, subtitle, business] = reservation.name.split(SEPARATOR);
+
+    const handleClick = () => {
+      if (isBusiness) return;
+
+      navigator.navigate("BusinessDetails", {
+        business: JSON.parse(business),
+      });
+    };
 
     return (
-      <TouchableOpacity
+      <TouchableScale
         style={[styles.item, { height: reservation.height }]}
-        onPress={() => Alert.alert(reservation.name)}
+        onPress={handleClick}
       >
-        <Text style={[styles.itemTitle, { fontSize, color }]}>
-          {data[0]}
-        </Text>
-        <Text style={[styles.itemSubtitle]}>{data[1]}</Text>
-      </TouchableOpacity>
+        <Text style={[styles.itemTitle, { fontSize, color }]}>{title}</Text>
+        <Text style={[styles.itemSubtitle]}>{subtitle}</Text>
+      </TouchableScale>
     );
   };
 
   const renderEmptyDate = () => {
     return (
       <View style={styles.emptyDate}>
-        <Text>This is empty date!</Text>
+        <Text>¡La fecha está vacía!</Text>
       </View>
     );
   };
 
   const renderEmptyData = () => {
     return (
-      <View style={styles.emptyDate}>
+      <Animatable.View useNativeDriver animation={'fadeInUp'} delay={DELAY} style={styles.emptyDate}>
+        <AnimatedLottieView
+          source={require("../../../../assets/lotties/empty-appointments-2.json")}
+          autoPlay
+          loop
+          speed={0.5}
+          style={{
+            height: width,
+            alignSelf: "flex-start",
+          }}
+          resizeMode="cover"
+        />
         <Text style={[styles.emptyDateText, { color: colors.text }]}>
           ¡No tienes citas este día!
         </Text>
-      </View>
+      </Animatable.View>
     );
   };
 
@@ -167,6 +209,10 @@ const AgendaComponent: FC<Props> = ({}) => {
       const error = exception as BaseErrorResponse;
       ToastAndroid.show(error.message, ToastAndroid.LONG);
     }
+  };
+
+  const rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+    return r1.name !== r2.name;
   };
 
   return (
@@ -189,8 +235,10 @@ const AgendaComponent: FC<Props> = ({}) => {
       // enable scroll
       scrollEnabled
       displayLoadingIndicator
+      //handle row change
+      rowHasChanged={rowHasChanged}
       // Initially selected day
-      selected={moment().format("YYYY-MM-DD")}
+      selected={today}
       showClosingKnob={true}
       // Max amount of months allowed to scroll to the past. Default = 50
       pastScrollRange={50}
@@ -259,7 +307,7 @@ const styles = StyleSheet.create({
   },
   emptyDateText: {
     fontFamily: theme.fonts.bold,
-    fontSize: theme.fontSizes.subHeading,
+    fontSize: theme.fontSizes.heading,
     alignSelf: "center",
   },
   itemTitle: {
