403e244d8cae00a99b3af8301ff97460001e5b5e
Adrián Coll <acoll@aiudo.es>
2022-06-08 01:15:04 +0200

    change endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,6 @@
 /** @var API_URL - Deployed backend application URI */
-// export const API_URL = 'https://stylish-backend.herokuapp.com/api'
+export const API_URL = 'https://stylish-backend.herokuapp.com/api'
 
-export const API_URL = 'http://192.168.1.6:8080/api'
-
+// export const API_URL = 'http://192.168.1.6:8080/api'
 export const REDIRECT_TIMEOUT = 5000
 export const GOOGLE_API_TOKEN = 'AIzaSyCUkqP7_EhGiT5A7XN1dPeXilkHg_5ktMU'
